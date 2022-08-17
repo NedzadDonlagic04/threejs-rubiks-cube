c83c91baa4d98977d79dea20c605ddf4e428b890
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-08-17 17:21:24 +0200

    Changed a lot
    The cube is not centered with the x,y and z axes
    (as can be seen currently with the axes helper
    object active).
    Using dat.gui added a gui dropdown that has buttons
    that when clicked take the user to the side of the
    cube they selected.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import {GUI} from 'dat.gui';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(85, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -9,25 +10,44 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(window.innerWidth,window.innerHeight);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-    color: 0x808080
-})
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+
+const material1 = new THREE.MeshBasicMaterial({
+    color: 0xFF3131
+});
+
+const material2 = new THREE.MeshBasicMaterial({
+    color: 0x7CFC00
+});
+
+const material3 = new THREE.MeshBasicMaterial({
+    color: 0x1F51FF
+});
+
+const material4 = new THREE.MeshBasicMaterial({
+    color: 0xFFEA00
+});
+
+const material5 = new THREE.MeshBasicMaterial({
+    color: 0xFF5733
+});
+
+const material6 = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF
+});
 
 const cubes = [];
 
-for(let z=0;z<3;z++)
+for(let z=-0.55;z<=0.55;z+=0.55)
 {
-    for(let y=0;y<3;y++)
+    for(let y=-0.55;y<=0.55;y+=0.55)
     {
-        for(let x=0;x<3;x++)
+        for(let x=-0.55;x<=0.55;x+=0.55)
         {
-            const cube = new THREE.Mesh(geometry, material);
+            const cube = new THREE.Mesh(geometry, [material1, material2, material3, material4, material5,material6]);
             
-            cube.position.setX(x * 1.1);
-            cube.position.setY(y * 1.1);
-            cube.position.setZ(z * 1.1);
-
+            cube.position.set(x, y, z);
+            
             scene.add(cube);
 
             cubes.push(cube);
@@ -44,7 +64,38 @@ scene.add(axesHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-camera.position.z = 6;
+camera.position.set(0, 4, 0);
+
+const gui = new GUI();
+gui.domElement.id = 'gui';
+
+const sides = {
+    top: () => {
+        camera.position.set(0, 4, 0)
+    },
+    bottom: () => {
+        camera.position.set(0, -4, 0)
+    },
+    front: () => {
+        camera.position.set(4, 0, 0)
+    },
+    back: () => {
+        camera.position.set(-4, 0, 0)
+    },
+    left: () => {
+        camera.position.set(0, 0, 4)
+    },
+    right: () => {
+        camera.position.set(0, 0, -4)
+    }
+};
+
+gui.add(sides, 'top').name('Top Side');
+gui.add(sides, 'bottom').name('Bottom Side');
+gui.add(sides, 'front').name('Front Side');
+gui.add(sides, 'back').name('Back Side');
+gui.add(sides, 'left').name('Left Side');
+gui.add(sides, 'right').name('Right Side');
 
 const animation = () => {
     requestAnimationFrame(animation);
