dd66b94daf1cd213030717ef52a699cc0aa0c52d
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-08-20 01:39:43 +0200

    Changed a lot
    Using TWEEN.js I managed to make the camera movement
    smoother when pressing the gui buttons, which were also
    moved into folders to make a distinction between options.
    Added and used a custom class called rubiksCube to represent
    the cube using OOP, using the fetch api managed to make it
    that with a press of a button the cube gets colored properly.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import {GUI} from 'dat.gui';
+import * as TWEEN from '@tweenjs/tween.js';
+import { RubiksCube } from './rubiksClass.mjs';
 
 
 /* First Step: Creating the 3 objects that will represent the base of our 3D section of the website */
@@ -23,67 +25,11 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth,window.innerHeight);
 
 
-/* Second Step: Creating all the cubes we'll need to represent the 3x3x3 rubik's cube */
+/* Second Step: Creating all the cubes we'll need to represent the 3x3x3 rubik's cube and adding helpers */
 
-// Creating an object that wil represent the cube, a box will the same width, height and depth
-const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-
-/* In this section we are creating 6 objects that represent the color that we will wrap around the cube */
-
-const material1 = new THREE.MeshBasicMaterial({
-    color: 0xFF3131
-});
-
-const material2 = new THREE.MeshBasicMaterial({
-    color: 0x7CFC00
-});
-
-const material3 = new THREE.MeshBasicMaterial({
-    color: 0x1F51FF
-});
-
-const material4 = new THREE.MeshBasicMaterial({
-    color: 0xFFEA00
-});
-
-const material5 = new THREE.MeshBasicMaterial({
-    color: 0xFF5733
-});
-
-const material6 = new THREE.MeshBasicMaterial({
-    color: 0xFFFFFF
-});
-
-// Creating an empty array which will hold all of the cubes that make up the rubik's cube
-const cubes = [];
-
-// Creating a variable which will save the distance between each cube
-// Because it will be reused a lot for the sake of an easier time for me I've made it a variable
-const distance = 0.52;
-
-// Nested for loop nested in another for loop
-// This is used to make our 3x3x3 rubik's cube appear like it does in the 3D space
-// The conditions are what they are so that the center of the 3D world is the central cube inside the rubik's cube
-for(let z=-distance;z<=distance;z+=distance)
-{
-    for(let y=-distance;y<=distance;y+=distance)
-    {
-        for(let x=-distance;x<=distance;x+=distance)
-        {
-            // By creating a mesh object which contains the geometry and materials (6 for 6 different sides of the cube) we create a single cube
-            const cube = new THREE.Mesh(geometry, [material1, material2, material3, material4, material5,material6]);
-            
-            // Setting the position of the cube
-            cube.position.set(x, y, z);
-            
-            // Adding the cube to the scene
-            scene.add(cube);
-
-            // Adding the cube to the end of our cubes array
-            cubes.push(cube);
-        }
-    }
-}
+// Making an instance of the class RubiksCube that I defined in another file
+// To it's constructor we are passing the scene to which it will add the cube, the side of the cube's cubes, and the location they will be placed on the x, y and z axes
+const rubiksCube = new RubiksCube(scene, 0.5, 0.52);
 
 /* Helper Section */
 // In this section I will keep the temporary helpers which don't affect the functionality of the code just add extra visual help to the 3D canvas
@@ -101,7 +47,7 @@ scene.add(axesHelper);
 /* Helper Section End */
 
 
-/* Third Step: Creating a way for our camera to move and a gui to allow us to move to a specific side of the cube */
+/* Third Step: Creating a way for our camera to move, a gui to allow us to move to a specific side of the cube and some animation to make it look more smooth */
 
 // Creating an OrbitControls object which will represent controls that allow movement of the camera
 // As argument to the constructor it takes the camera and the document element of the renderer
@@ -110,53 +56,128 @@ const controls = new OrbitControls(camera, renderer.domElement);
 // Setting the position of our camera
 camera.position.set(0, 4, 0);
 
-// Creating an object that will represent our GUI
-// It will position itself automatically to the top right corner
-const gui = new GUI();
+// Making an arrow function that will be used to create our tween
+// To quote the documentation
+// "A tween (from in-between) is a concept that allows you to change the values of the properties of an object in a smooth way"
+// And it is exactly what we are doing here for the passed coordinates to where we want the camera to be at the end of the animation we will take the current position and slightly shift it little by little until it gets to the desired coordinate
+const makeTween = (posX, posY, posZ) => {
+
+    const camPos = camera.position;
+    
+    // Creating our tween which is an instance of the Tween class
+    // It's constructor takes the starting coordinates and using the to method we can add the ending coordinates as well as the delay for the animation
+    const tween = new TWEEN.Tween({x: camPos.x, y: camPos.y, z: camPos.z}).to({x: posX, y: posY, z: posZ}, 500);
+    
+    // In order to see these changes we need to update them little by little on the camera, using the onUpdate method we can pass a callback which will be called each time the update function is called
+    tween.onUpdate( ({
+        x,y,z
+    }) => {
+        camera.position.set(x, y, z);
+    });
+
+    // Using the start method we start the tween
+    tween.start();
+}
 
 // Creating an object called sides
 // This object contains properties which all have a name that represent a side of the cube
 // The values they store are all arrow functions that set the camera position to the same one as the name
-// This object will be useful later
+// Each arrow function will call the previously made makeTween function with just different coordinates
 const sides = {
     top: () => {
-        camera.position.set(0, 4, 0)
+        makeTween(0, 4, 0);
     },
     bottom: () => {
-        camera.position.set(0, -4, 0)
+        makeTween(0, -4, 0);
     },
     front: () => {
-        camera.position.set(4, 0, 0)
+        makeTween(4, 0, 0);
     },
     back: () => {
-        camera.position.set(-4, 0, 0)
+        makeTween(-4, 0, 0);
     },
     left: () => {
-        camera.position.set(0, 0, 4)
+        makeTween(0, 0, 4);
     },
     right: () => {
-        camera.position.set(0, 0, -4)
+        makeTween(0, 0, -4);
     }
 };
 
-// Using the previously created gui object we can add buttons that each when pressed will take the user to the respective side of the cube
-// By calling the add method we add an action to the gui, the action is based on the initial value we pass, in this case we pass an object which has each property hold an arrow function, because it's a function it will be seen as a button
+// Creating an object that will represent our GUI
+// It will position itself automatically to the top right corner
+const gui = new GUI();
+
+// Adding 2 folders to our gui
+const changePos = gui.addFolder('Change Position');
+const toggleOptions = gui.addFolder('Toggle Options');
+
+// Using the previously created changePos object (that represents a folder) we can add buttons that each when pressed will take the user to the respective side of the cube
+// By calling the add method we add an action to changePos, the action is based on the initial value we pass, in this case we pass an object which has each property hold an arrow function, because it's a function it will be seen as a button
 // The first argument is the object the second is the property name
 // Additionally we can call the name method right after to give that button a fitting description, if we don't do this the text will be the property name we passed
-gui.add(sides, 'top').name('Top Side');
-gui.add(sides, 'bottom').name('Bottom Side');
-gui.add(sides, 'front').name('Front Side');
-gui.add(sides, 'back').name('Back Side');
-gui.add(sides, 'left').name('Left Side');
-gui.add(sides, 'right').name('Right Side');
+changePos.add(sides, 'top').name('Top Side');
+changePos.add(sides, 'bottom').name('Bottom Side');
+changePos.add(sides, 'front').name('Front Side');
+changePos.add(sides, 'back').name('Back Side');
+changePos.add(sides, 'left').name('Left Side');
+changePos.add(sides, 'right').name('Right Side');
+
+// Using the previously created toggleOptions object (that represents a folder) we can add a checkbox that when ticked will turn on the axes helpers and when ticked off will turn them off
+// As previously mentioned the action type is based on the initial value we pass, since this property is a boolean it turns it into a checkbox
+toggleOptions.add(axesHelper, 'visible').name('Toggle Axes');
+
+// By default our gui will be closed
+gui.close();
+
+
+/* Forth Step: Using a test api to help us mimic the way it would be received in practice */
+
+// Defining an async arrow function that will have a parameter called index
+// It will first use the fetch api to send a request to an api, take the response and check is it ok
+// If so it will return the json object, if not it will return a Promise.reject() object which will trigger the catch block
+// Lastly we check is the json undefined, if not we will return the given part of the json based on the passed index
+const getter = async index => {
+    const json = await fetch('https://62e596abde23e2637921e0e5.mockapi.io/colors')
+        .then( response => {
+            if(response.ok)
+            {
+                return response.json();
+            }
+            return Promise.reject();
+        })
+        .catch( error => console.log(`Error: ${error}`));
+
+    if(json!==undefined)
+    {
+        return json[index];
+    }
+}
+
+// Defining an object called fetcher with a get property which will have an async arrow function 
+// Inside, it will go through a for in which it will call the previously defined getter function and receive the proper json which it will pass to the color method of the rubiksCube object which will in turn color all the sides of the rubik's cube properly
+const fetcher = {
+    get: async() => {
+        for(let i=0; i<6; i++)
+        {
+            const json = await getter(i);
+            rubiksCube.color(json);
+        }
+    }
+}
+
+// Adding the fetcher object as a button in the gui for easier use
+toggleOptions.add(fetcher, 'get').name('Scan');
+
 
 /* Final Step: Creating the function that will do the animation of the 3D canvas and adding all the event listeners */
 
-// Defining an arrow function called animation, which will when called call the requestAnimationFrame function and give itself as a callback, after which it will update the controls and render the scene again
+// Defining an arrow function called animation, which will when called call the requestAnimationFrame function and give itself as a callback, after which it will update the controls, call the update function and render the scene again
 // The requestAnimationFrame function is asynchronous so the code under it will execute before it calls the callback it's passed
 const animation = () => {
     requestAnimationFrame(animation);
     controls.update();
+    TWEEN.update();
     renderer.render(scene, camera);
 }
 
@@ -168,7 +189,7 @@ window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth/window.innerHeight;
     // Whenever a change is made to the camera after the initial constructor call the updateProjectionMatrix needs to be called to update it
     camera.updateProjectionMatrix();
-
+    
     renderer.setSize(window.innerWidth,window.innerHeight);
     renderer.render(scene, camera);
 });
